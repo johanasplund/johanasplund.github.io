7a916a0458a1183d2fab0b2dffc9517a427c4adf
johanasplund <johanasplund@live.se>
2014-08-06 03:31:53 +0200

    Reinstall octoenv

--- octoenv/bin/activate.fish
+++ octoenv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/asplund/Documents/octo/octoenv"
+set -gx VIRTUAL_ENV "/Users/asplund/Documents/johanasplund.github.io/octoenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
